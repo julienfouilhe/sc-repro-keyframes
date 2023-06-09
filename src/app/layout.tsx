dcb475acf2f1059ab78850fd73724fb61362b8d8
Julien Fouilhé <julien.fouilhe@gmail.com>
2023-06-09 19:11:39 +0200

    Use recommended settings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import StyledComponentsRegistry from '@/lib/StyledComponentsRegistry'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -14,7 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <StyledComponentsRegistry>
+          {children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
